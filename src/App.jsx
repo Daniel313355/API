fa704eaff91ab1dfb43f53dc41c80a43302b28fd
Daniel313355 <daniel.vieda@uniagustiniana.edu.co>
2025-04-03 02:20:13 +0000

    r

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Aleatorios from './Componentes/Aleatorios'
 import Detalle from './Componentes/Detalle'
 import Favoritos from './Componentes/Favoritos'
 import Listar from './Componentes/Listar'
+import Menu from './Componentes/Menu'
 import Original from './Componentes/Original'
 import Usuarios from './Componentes/Usuarios'
 
 function App() {
 
   return (
-    <>
-      <Aleatorios />
-      <Detalle />
-      <Favoritos />
-      <Listar />
-      <Original />
-      <Usuarios />
-    </>
+    <Router>
+      <Menu />
+      <Routes>
+
+      </Routes>
+    </Router>
+  
   )
 }
 
