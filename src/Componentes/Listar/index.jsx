232280c14ee7b36a19f1706d75e746d7c2e99f22
Daniel313355 <daniel.vieda@uniagustiniana.edu.co>
2025-04-10 01:45:34 +0000

    tIPOS

--- src/Componentes/Listar/index.jsx
+++ src/Componentes/Listar/index.jsx
@@ -1,11 +1,47 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import './style.css'
+import Filtro from '../Filtros';
 
 function Listar() {
 
+    const [data, setData] = useState([]);
+    const [busqueda, setBusqueda] = useState('');
+    const [tipoSeleccionado, setTipoSeleccionado] = useState('All');
+  
+
+  useEffect(() => {
+    const obtenerDatos = async () => {
+      if (tipoSeleccionado === 'All') {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1025");
+        const json = await res.json();
+        setData(json.results);
+      } else {
+        const res = await fetch(`https://pokeapi.co/api/v2/type/${tipoSeleccionado}`);
+        const json = await res.json();
+        const listaFiltrada = json.pokemon.map(p => p.pokemon);
+        setData(listaFiltrada);
+      }
+    };
+
+    obtenerDatos();
+  }, [tipoSeleccionado]);
+
   return (
     <>
-      <h1>Listar</h1>
+    <Filtro />
+      <section className='c-lista'>
+
+{data.map((pokemon, index) => (
+  <div className='c-lista-pokemon'
+  key={index}>
+    <p>{pokemon.url.split("/")[6]}</p>
+    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.url.split("/")[6]}.png`} 
+          alt={`Pokémon ${pokemon.name}`} width='auto' height='60' loading='lazy'
+        />
+    <p>{pokemon.name}</p>
+  </div>
+))}
+</section>
     </>
   )
 }
